08fd23b371499c32560f8a9443cfb4ade2246bd5
abhro <5664668+abhro@users.noreply.github.com>
2025-04-02 15:44:06 -0400

    Add analytical moments for normal and binormal distributions

--- src/moments.jl
+++ src/moments.jl
@@ -24,3 +24,29 @@ function centralmoments(x::AbstractVector, n::Integer)
     end
     return m
 end
+
+moment(d::BiNormal, k) = d.λ*moment(d.N₁, k) + (1-d.λ)*moment(d.N₂, k)
+
+moment(d::Normal, ::Val{1}) = d.μ
+moment(d::Normal, ::Val{2}) = d.μ^2 + d.σ^2
+moment(d::Normal, ::Val{3}) = d.μ * (d.μ^2 + 3*d.σ^2)
+function moment(d::Normal, ::Val{4})
+    μ, σ = params(d)
+    return μ^4 + 6*μ^2*σ^2 + 3*σ^4
+end
+function moment(d::Normal, ::Val{5})
+    μ, σ = params(d)
+    return μ^5 + 10*μ^3*σ^2 + 15*μ*σ^4
+end
+function moment(d::Normal, ::Val{6})
+    μ, σ = params(d)
+    return μ^6 + 15*μ^5*σ^2 + 45*μ^2*σ^4 + 15*σ^6
+end
+function moment(d::Normal, ::Val{7})
+    μ, σ = params(d)
+    return μ^7 + 21*μ^5*σ^2 + 105*μ^3*σ^4 + 105*σ^7
+end
+function moment(d::Normal, ::Val{8})
+    μ, σ = params(d)
+    return μ^8 + 28*μ^6*σ^2 + 210*μ^4*σ^4 + 420*μ^2*σ^6 + 105*σ^8
+end
