aada2fe4f99ca785b8da3fc235fa0d25a45819a4
abhro <5664668+abhro@users.noreply.github.com>
2025-04-02 15:19:51 -0400

    Create moments.jl

--- src/BiNormalDistribution.jl
+++ src/BiNormalDistribution.jl
@@ -226,33 +226,6 @@ The mathematical definition is:
 """
 Distributions.cf(d::BiNormal, ::Any) = λ*cf(d.N₁, t) + (1-λ)*cf(d.N₂, t)
 
-@doc raw"""
-    moments(x::AbstractVector, n::Integer)
-
-Get the first `n`-moments of a given dataset. (``⟨ x^k ⟩``, ``k = 1, \dots, n``)
-"""
-function moments(x::AbstractVector, n::Integer)
-    m = zeros(eltype(x), n)
-    for k in eachindex(m)
-        m[k] = mean(xᵢ -> xᵢ^k, x)
-    end
-    return m
-end
-@doc raw"""
-    centralmoments(x::AbstractVector, n::Integer)
-
-Get the first `n` central moments of a given dataset. (``⟨ (x-μ)^k ⟩``, ``k = 1, \dots, n``)
-"""
-function centralmoments(x::AbstractVector, n::Integer)
-    m = zeros(eltype(x), n)
-    μ = mean(x)
-    m[begin] = μ
-    for k in eachindex(m)[begin+1:end]
-        m[k] = mean(xᵢ -> (xᵢ-μ)^k, x)
-    end
-    return m
-end
-
 function Base.show(io::IO, d::BiNormal)
     λ, μ₁, σ₁, μ₂, σ₂ = params(d)
     print(io, typeof(d)) # handle type parameters
@@ -278,5 +251,6 @@ function Distributions.median(d::BiNormal)
 end
 
 include("fitters.jl")
+include("moments.jl")
 
 end
