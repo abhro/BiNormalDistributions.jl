429f112e79dfd94b9f69f430966b7e158e5e7b6f
abhro <5664668+abhro@users.noreply.github.com>
2025-04-23 06:14:23 -0400

    Add test for same underlying means

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,27 @@
 using BiNormalDistribution
 using Test
 using Aqua
+using StableRNGs
+using Distributions: Uniform
 
 @testset "BiNormalDistribution" begin
     @testset "Code quality (Aqua)" begin
         Aqua.test_all(BiNormalDistribution, piracies = false)
     end
-    # Write your tests here.
 
     # test distribution with same underlying means
+    @testset "μ₁ = μ₂ = μ" begin
+        rng = StableRNG(123)
+        μ = rand(rng)
+        λ = rand(rng, Uniform(1//2, 1))
+        @info "Testing distributions with same mean $μ (λ = $λ)"
+        dist = BiNormal(λ, μ, 1, μ, 1)
+
+        # Create a dataset with "enough" samples
+        x = rand(rng, dist, 1000)
+
+        @test mean(x) ≈ μ
+    end
 
     # ditto with same variances
 end
